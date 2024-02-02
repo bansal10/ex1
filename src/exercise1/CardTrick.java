1dd32965f07317594f9c9f18371ea9ba6dde1d6a
Aman Aman <aman10@sheridancollege.ca>
2024-02-02 16:02:57 -0500

    change it

--- src/exercise1/CardTrick.java
+++ src/exercise1/CardTrick.java
@@ -41,7 +41,7 @@ public class CardTrick {
      * @author Paul Bonenfant Jan 2022
      */
     private static void printInfo() {
-    
+        System.out.println("Aman Aman");
         System.out.println("Congratulations, you guessed right!");
         System.out.println();
         
